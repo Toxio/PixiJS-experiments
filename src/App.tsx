c6e30420c29ee1e0367eda3f7b9691b70fc9a2b4
toha <toxio.xo@gmail.com>
2026-05-04 20:16:17 +0200

    Clean code

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,6 @@
-import { useState } from 'react';
-import { Header, type TabId } from './components/Header';
-import { DemoScene } from './components/DemoScene';
-import { SlotsScene } from './components/SlotsScene';
-import { BlastScene } from './components/BlastScene';
-import { ExplosionScene } from './components/ExplosionScene';
-import { GloveScene } from './components/GloveScene';
-import { WinFrameScene } from './components/WinFrameScene';
-import './App.css';
-
+import OldExperimentsApp from './old/OldExperimentsApp';
 function App() {
-  const [tab, setTab] = useState<TabId>('slots');
-
-  return (
-    <div className="app-layout">
-      <Header active={tab} onChange={setTab} />
-      <main className="app-content">
-        {tab === 'slots' && <SlotsScene />}
-        {tab === 'balls' && <DemoScene />}
-        {tab === 'frame' && <WinFrameScene />}
-        {tab === 'explosion' && <ExplosionScene />}
-        {tab === 'blast' && <BlastScene />}
-        {tab === 'glove' && <GloveScene />}
-      </main>
-    </div>
-  );
+  return <OldExperimentsApp />;
 }
 
 export default App;
---
+++ src/old/OldExperimentsApp.tsx
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+import { Header, type TabId } from './components/Header';
+import { DemoScene } from './components/DemoScene';
+import { SlotsScene } from './components/SlotsScene';
+import { BlastScene } from './components/BlastScene';
+import { ExplosionScene } from './components/ExplosionScene';
+import { GloveScene } from './components/GloveScene';
+import { WinFrameScene } from './components/WinFrameScene';
+import './OldExperimentsApp.css';
+
+export default function OldExperimentsApp() {
+  const [tab, setTab] = useState<TabId>('slots');
+
+  return (
+    <div className="app-layout">
+      <Header active={tab} onChange={setTab} />
+      <main className="app-content">
+        {tab === 'slots' && <SlotsScene />}
+        {tab === 'balls' && <DemoScene />}
+        {tab === 'frame' && <WinFrameScene />}
+        {tab === 'explosion' && <ExplosionScene />}
+        {tab === 'blast' && <BlastScene />}
+        {tab === 'glove' && <GloveScene />}
+      </main>
+    </div>
+  );
+}
