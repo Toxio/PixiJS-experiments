838fb2c4e7b1a8658b813b9544709aa8f7e49fdb
toha <toxio.xo@gmail.com>
2026-05-05 00:06:12 +0200

    Clean

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,29 +1,27 @@
 import reelImage from '../assets/reel.png';
-import { Glass } from './Glass';
-import { Goblet } from './Goblet';
-import { Lipstick } from './Lipstick';
-import { Lips } from './Lips';
-import { Parfume } from './Parfume';
-import { Rose } from './Rose';
-import { Seven } from './Seven';
-import { Star } from './Star';
-import { Wild } from './Wild';
+import {Glass} from './Glass';
+import {Goblet} from './Goblet';
+import {Lipstick} from './Lipstick';
+import {Lips} from './Lips';
+import {Parfume} from './Parfume';
+import {Rose} from './Rose';
+import {Seven} from './Seven';
+import {Star} from './Star';
 
 export function Main() {
-  return (
-    <main className="main-screen">
-      <div className="main-reel-wrap">
-        <img className="main-reel" src={reelImage} alt="Reel" decoding="async" />
-        <Glass />
-        <Goblet />
-        <Lips />
-        <Lipstick />
-        <Parfume />
-        <Rose />
-        <Seven />
-        <Star />
-        <Wild />
-      </div>
-    </main>
-  );
+    return (
+        <main className="main-screen">
+            <div className="main-reel-wrap">
+                <img className="main-reel" src={reelImage} alt="Reel" decoding="async"/>
+                <Glass/>
+                <Goblet/>
+                <Lips/>
+                <Lipstick/>
+                <Parfume/>
+                <Rose/>
+                <Seven/>
+                <Star/>
+            </div>
+        </main>
+    );
 }
